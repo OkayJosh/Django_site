ee2a551e9d42c89463f7a8c3598d568047fa82a7
Olatunji Joshua <19627177+OkayJosh@users.noreply.github.com>
2018-06-28 19:18:04 +0100

    modified

--- polls/models.py
+++ polls/models.py
@@ -19,8 +19,6 @@ class Question(models.Model):
     def was_pubulish_recently(self):
         return self.pub_date >= timezone.now - datetime.timedelta(days=1)
 
-
-
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
