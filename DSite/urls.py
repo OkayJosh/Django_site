523c33a34e1d715b44d15905744f36990913e67d
Olatunji Joshua <19627177+OkayJosh@users.noreply.github.com>
2018-06-28 19:17:30 +0100

    modified

--- DSite/urls.py
+++ DSite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path(r'polls/', include('polls.url')),
+    path(r'polls/', include('polls.urls')),
     path(r'admin/', admin.site.urls),
 ] 
