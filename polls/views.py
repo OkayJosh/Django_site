75b39ebce0e376599f41e02f9c3c3d35aacca355
Olatunji Joshua <19627177+OkayJosh@users.noreply.github.com>
2018-06-30 14:58:07 +0100

    modified

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 
@@ -9,15 +9,15 @@ from .models import Question, Choice
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_objext_name = 'latest_question_list'
+    context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         "Return the last publish Questions"
         return Question.objects.order_by('-pub_date')[:5]
 
-class DetailView (generic.DeleteView):
+class DetailView (generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html',
+    template_name = 'polls/detail.html'
     
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
